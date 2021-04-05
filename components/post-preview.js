092991774d3a3a6e5fc59a8729ad20fd65ec0629
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-05 13:26:59 +0000

    Update hyperlink hiearchy

--- components/post-preview.js
+++ components/post-preview.js
@@ -22,7 +22,7 @@ export default function PostPreview({
         />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
+        <Link as={`/${slug}`} href="/[slug]">
           <a className="hover:underline">{title}</a>
         </Link>
       </h3>
--- pages/[slug].js
+++ pages/[slug].js
@@ -1,14 +1,14 @@
 import { useRouter } from 'next/router';
 import ErrorPage from 'next/error';
-import Container from '../../components/container';
-import PostBody from '../../components/post-body';
-import Header from '../../components/header';
-import PostHeader from '../../components/post-header';
-import Layout from '../../components/layout';
-import { getPostBySlug, getAllPosts } from '../../lib/api';
-import PostTitle from '../../components/post-title';
+import Container from '../components/container';
+import PostBody from '../components/post-body';
+import Header from '../components/header';
+import PostHeader from '../components/post-header';
+import Layout from '../components/layout';
+import { getPostBySlug, getAllPosts } from '../lib/api';
+import PostTitle from '../components/post-title';
 import Head from 'next/head';
-import markdownToHtml from '../../lib/markdownToHtml';
+import markdownToHtml from '../lib/markdownToHtml';
 
 export default function Post({ post, morePosts, preview }) {
   const router = useRouter()
