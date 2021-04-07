78bbfb2d2db6dca1b375027c6d33f681e346a51c
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-07 14:57:32 +0000

    Proper footer copyright

--- components/footer.js
+++ components/footer.js
@@ -12,7 +12,7 @@ export default function Footer() {
             <a
               href="https://github.com/filiptronicek"
               target="_blank"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="mx-3 bg-black dark:text-blue hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               My GitHub
             </a>
@@ -25,7 +25,9 @@ export default function Footer() {
             </a>
           </div>
         </div>
-        ©{new Date().getFullYear()} Filip Troníček. All rights reserved.
+        <div className="grid justify-items-center pb-8">
+          ©{new Date().getFullYear()} Filip Troníček. All rights reserved.
+        </div>
       </Container>
     </footer>
   )
