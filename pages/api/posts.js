ec007b1a91700f6fe75f700fef661d24510a5df2
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-08 15:35:57 +0000

    Posts API

---
+++ pages/api/posts.js
@@ -0,0 +1,16 @@
+import { getAllPosts } from '../../lib/api';
+
+export default (_req, res) => {
+    res.statusCode = 200;
+    (async function () {
+        const allPosts = getAllPosts([
+            'title',
+            'date',
+            'slug',
+            'author',
+            'coverImage',
+            'excerpt',
+        ]);
+        res.json({ name: allPosts })
+    }());
+}
