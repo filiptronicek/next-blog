14176cdc21c11d299696de55952a9bb2f25ff2a0
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-11 19:18:50 +0000

    ;

--- components/hero-post.js
+++ components/hero-post.js
@@ -1,6 +1,6 @@
-import DateFormatter from '../components/date-formatter'
-import CoverImage from '../components/cover-image'
-import Link from 'next/link'
+import DateFormatter from '../components/date-formatter';
+import CoverImage from '../components/cover-image';
+import Link from 'next/link';
 
 export default function HeroPost({
   title,
@@ -12,31 +12,31 @@ export default function HeroPost({
   return (
     <section>
       <div className="md:flex">
-      <div className="mb-8 md:mb-16 w-100% lg:w-1/2">
-        <CoverImage
-          title={title}
-          src={coverImage}
-          slug={slug}
-          height={620}
-          width={1240}
-          loading="eager"
-        />
-      </div>
-      <div className="md:ml-8 mb-20 md:mb-28">
-        <div>
-          <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-            <Link as={`/${slug}`} href="/[slug]">
-              <a className="hover:underline">{title}</a>
-            </Link>
-          </h3>
-          <div className="mb-4 md:mb-0 text-lg">
-            <DateFormatter dateString={date} />
+        <div className="mb-8 md:mb-16 w-100% lg:w-1/2">
+          <CoverImage
+            title={title}
+            src={coverImage}
+            slug={slug}
+            height={620}
+            width={1240}
+            loading="eager"
+          />
+        </div>
+        <div className="md:ml-8 mb-20 md:mb-28">
+          <div>
+            <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
+              <Link as={`/${slug}`} href="/[slug]">
+                <a className="hover:underline">{title}</a>
+              </Link>
+            </h3>
+            <div className="mb-4 md:mb-0 text-lg">
+              <DateFormatter dateString={date} />
+            </div>
+          </div>
+          <div>
+            <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
           </div>
         </div>
-        <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-        </div>
-      </div>
       </div>
     </section>
   )
